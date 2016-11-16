8ada6ddb9242883c2d066e00c7b74e44428cc1d0
Alan Tran <trannhatlan@gmail.com>
2016-11-15 23:38:28 -0700

    add directive using ngModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import { FormsModule } from '@angular/forms';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, CustomerComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
