807c8b29a1e19bccee814c0652f24591e4e73f1a
Alan Tran <atran@arrow.com>
2016-11-15 22:19:09 -0700

    push to github

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,7 +8,13 @@ export class AppComponent {
     title = 'Customer App';
     name = 'Alan';
     wardsColor = 'blue';
-
+    customers = [
+        {id: 1, name:'Ward'},
+        {id: 2, name:'Ken'},
+        {id: 3, name:'Eric'},
+        {id: 4, name:'Sally'},
+        {id: 5, name:'Emmet'}
+    ];
     changeSuitColor() {
         this.wardsColor = this.wardsColor === 'blue' ? 'red' : 'blue';
     }
