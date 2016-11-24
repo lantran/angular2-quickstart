5102d07c4244387fee5ff3f37a1b56a5944a6f53
Alan Tran <atran@arrow.com>
2016-11-24 09:48:06 -0500

    got Rx working, catch error

--- app/customer/customer.service.ts
+++ app/customer/customer.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from "@angular/http";
+import { Observable } from 'rxjs/Rx';
 
 const URL_CUSTOMER = 'app/customers.json';
 
@@ -10,7 +11,14 @@ export class CustomerService {
     
     getCustomers(){
        return this._http.get(URL_CUSTOMER)
-            .map((response: Response) => response.json());
+            .map((response: Response) => response.json())
+            .catch(this._handlerError);
     }
+
+    _handlerError(err: any) {
+        console.log(err);
+        return Observable.throw(err);
+    }
+
 
 }
