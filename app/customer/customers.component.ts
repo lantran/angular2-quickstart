723d8b64ad16695eb2589783113fd8e18f9e095c
Alan Tran <atran@arrow.com>
2016-11-24 14:04:47 -0500

    Rx with subscribe

--- app/customer/customers.component.ts
+++ app/customer/customers.component.ts
@@ -15,14 +15,24 @@ export class CustomersComponent implements OnInit {
 
     constructor(private _customerService: CustomerService) { }
 
-    ngOnInit() { 
-        this._customerService.getCustomers()
-            .then((customers) => this.customers = customers)
-            .catch(err => {
-                console.log(err);
-                // return Observable.of(err);
-            });
-            
+    ngOnInit() {
+        // Rx observable version with subscribe function to a customer array
+        this._customerService.getCustomers_RxObservable()
+            .subscribe(
+                // if works
+                (customers) => this.customers = customers,
+                // error
+                (err) => { console.log(err);}
+            );
+
+        // Straight up promise to array 
+        // this._customerService.getCustomers()
+        //     .then((customers) => this.customers = customers)
+        //     .catch(err => {
+        //         console.log(err);
+        //         // return Observable.of(err);
+        //     });
+
         // Promise<any[]>
         // this.customers = this._customerService.getCustomers()
         //     .catch(err => {
@@ -30,7 +40,7 @@ export class CustomersComponent implements OnInit {
         //     });
 
         // Rx observable version
-        //  this.customers = this._customerService.getCustomers()
+        //  this.customers = this._customerService.getCustomers_RxObservable()
         //     .catch(err => {
         //         console.log(err);
         //         return Observable.of(err);
